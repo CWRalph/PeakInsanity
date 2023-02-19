f339c7dbc17ed0ae15b2802026dc13c44d0ae033
CWRalph <colwynralph15@gmail.com>
2023-02-18 17:25:11 -0800

    Map stuffs

--- src/app/core/main-map/main-map.component.ts
+++ src/app/core/main-map/main-map.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+
+export interface MapDimensions{
+  xpos:number;
+  ypos:number;
+  width:number;
+  height:number;
+}
 
 @Component({
   selector: 'app-main-map',
   templateUrl: './main-map.component.html',
   styleUrls: ['./main-map.component.scss']
 })
-export class MainMapComponent {
+export class MainMapComponent implements OnInit{
+  @ViewChild('map-image') mapImage!:ElementRef;
+  
+  mapDimensions?:MapDimensions;
 
+  ngOnInit(): void {
+    console.log("Map launched")
+  }
+  
+
+  setMapDimensions(dimensions:MapDimensions){
+    this.mapDimensions = dimensions;
+  }
 }
+
